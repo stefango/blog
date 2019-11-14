870bc24e79a15018ef059cf0f96eadc39031d0a7
Xu Zili <stefango@qq.com>
2019-11-14 18:06:37 +0800

    keep terminal alive after executing the shell

--- quick_push.sh
+++ quick_push.sh
@@ -1,5 +1,9 @@
+echo "<---git status"
+git status
+echo "git status--->"
 read -p "Plz input your comment: " comment
 echo "Your comment is $comment"
 git add .
 git commit -m "$comment"
 git push
+read
